7affaa315675ff0b00ac86ca2440302447f4db54
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2014-12-26 01:23:20 -0600

    backend setup.

--- database/schemas/book.js
+++ database/schemas/book.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Book = new mongoose.Schema({
   title: String,
   author: String,
-  start: Date,
-  end: Date,
-  progress: Number
+  readStart: Date,
+  readEnd: Date,
+  readProgress: Number
 });
 
 module.exports = Book;
