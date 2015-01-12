38d8f400c31aab336e907724c9e5f3fd0770c650
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2015-01-11 21:51:51 -0600

    initial books screen.

--- public/scripts/views/booksView.js
+++ public/scripts/views/booksView.js
@@ -5,9 +5,7 @@ define(function (require, exports, module) {
     _ = require('underscore'),
     Backbone = require('backbone'),
     tmp = require('hb!templates/mybooks.hbs'),
-    area_tmp = require('hb!templates/mybooks_area.hbs'),
-    table_tmp = require('hb!templates/mybooks_table.hbs'),
-    form_tmp = require('hb!templates/mybooks_form.hbs');
+    table_tmp = require('hb!templates/mybooks_table.hbs');
 
   return Backbone.View.extend({
     initialize: function (options) {
@@ -18,25 +16,12 @@ define(function (require, exports, module) {
       });
     },
 
-    getHeaders: function (dataArray) {
-      var hcount = 0,
-        headers = [];
-      _.each(dataArray, function (object) {
-        var objectKeys = _.keys(object);
-        if (objectKeys.length > hcount) {
-          headers = objectKeys;
-          hcount = objectKeys.length;
-        }
-      });
-      return headers;
-    },
-
     booksError: function (collection, response, options) {
       console.error(response);
     },
+
     render: function () {
       this.$el.html(tmp());
-      this.$el.append(form_tmp());
       this.$el.append(table_tmp({
         books: this.books.models
       }));
